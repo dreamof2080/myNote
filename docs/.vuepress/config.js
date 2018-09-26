7ac3fb984a28135ba0cc9165d2417c836e765045
906956368@qq.com <906956368@qq.com>
2018-09-26 21:33:06 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    title:'知识文档',
+    description:'总结知识，分享知识',
+    base:'/',
+    host:'0.0.0.0',
+    port:8081,
+    //额外的需要被注入到当前页面的HTML head中的标签，其中路径的'/'就是public资源目录
+    head:[
+        ['link',{rel:'icon',href:'/img/logo.ico'}],
+        ['link',{rel:'manifest',href: '/manifest.json'}],
+    ],
+    themeConfig:{
+        //gitc仓库地质
+        repo:'https://github.com/dreamof2080/myNote.git',
+        //导航栏
+        nav:[
+            {text:'Home',link:'/'},
+            {text:'后台',
+                items:[
+                    {text:'java',link:'/backStage/java/'},
+                    {text:'框架',link:'/backStage/frameWork/'},
+                ]
+            },
+            {text:'数据库',
+                items:[
+                    {text:'oracle',link:'/db/oracle/'},
+                    {text:'mysql',link:'/db/mysql/'},
+                ]
+            },
+            {text:'前端',
+                items:[
+                    {text:'vue.js',link:'/front/vuejs/'},
+                    {text:'设计',link:'/front/design/'},
+                ]
+            },
+            {text:'linux',link:'/linux/'},
+        ],
+        search:true,
+    }
+};
---
+++ docs/backStage/java/README.md
@@ -0,0 +1,10 @@
+---
+sidebar: auto
+---
+
+
+# java
+
+#### 201805
+- [记Windows开发迁移至Mac环境](./记Windows开发迁移至Mac环境.md)
+- [VuePress搭建个人blog](./VuePress搭建个人blog.md)
